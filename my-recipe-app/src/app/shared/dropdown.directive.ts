69ddaf1c6c465a434df41baeccf013d434ab71fb
aduyinuo <yinuod@andrew.cmu.edu>
2020-03-21 21:57:58 -0400

    Try again to push angular example

---
+++ my-recipe-app/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+    selector: '[appDropDown]'
+})
+export class DropdownDirective {
+    @HostBinding('class.open') isOpen = false;
+
+    @HostListener('click') toggleOpen() {
+        this.isOpen = !this.isOpen;
+    }
+}
