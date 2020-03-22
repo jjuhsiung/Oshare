83126db7f3298c93c299c7e361fef49a0787fa3b
Tian Su <tians@andrew.cmu.edu>
2020-03-22 19:05:25 -0400

    add Post, PostImage and Comment models and add the MEDIA_URL to the settings

--- django/webapps/oshare/models.py
+++ django/webapps/oshare/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    likes = models.IntegerField(default=0)
+    text = models.CharField(max_length=1500)
+    products = models.ManyToManyField('Product')
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, related_name='images')
+    image = models.ImageField(upload_to='oshare/post_img', null=True, blank=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    text = models.CharField(max_length=300)
--- django/webapps/webapps/settings.py
+++ django/webapps/webapps/settings.py
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
