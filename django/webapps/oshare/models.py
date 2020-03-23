caba8de7c9c6bbe34af02b4f3205233422a9f43d
aduyinuo <yinuod@andrew.cmu.edu>
2020-03-22 21:45:29 -0400

    order and cart

--- django/webapps/oshare/models.py
+++ django/webapps/oshare/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Order(models.Model):
+	user = models.ForeignKey('User', on_delete=models.CASCADE)
+	total = models.IntegerField(default=0)
+	ship_addr = models.CharField(max_length=100)
+	order_time = models.DateTimeField()
+
+class Cart(models.Model):
+	user = models.ForeignKey('User', on_delete=models.CASCADE)
+	total = models.IntegerField(default=0)
+		
