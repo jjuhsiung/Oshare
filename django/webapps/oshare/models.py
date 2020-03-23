bb0c5e5498483b66c733ca0119b2338947f771e3
JuiHsuan-Kuo <fionakuo26@gmail.com>
2020-03-22 21:40:19 -0400

    finish Product and ProductCount model

--- django/webapps/oshare/models.py
+++ django/webapps/oshare/models.py
@@ -32,7 +32,6 @@ class Post(models.Model):
     text = models.CharField(max_length=1500)
     products = models.ManyToManyField('Product')
 
-
 class PostImage(models.Model):
     post = models.ForeignKey(Post, related_name='images')
     image = models.ImageField(upload_to='oshare/post_img', null=True, blank=True)
@@ -43,4 +42,16 @@ class Comment(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE)
     date = models.DateTimeField()
     text = models.CharField(max_length=300)
+
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    brand = models.CharField(max_length=300)
+    price = models.FloatField(default=0)
+    description = models.CharField(max_length=400)
+    image = models.ImageField(upload_to='oshare/product_img', null=True, blank=True)
+    rating = models.FloatField(default=0)
+
+class ProductCount(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    count = models.IntegerField(default=0)
 
