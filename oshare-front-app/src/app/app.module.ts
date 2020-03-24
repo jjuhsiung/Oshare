c39a9c25be3988814f01e85484f6bdab0e4ea58e
Tian Su <tians@andrew.cmu.edu>
2020-03-24 01:24:37 -0400

    add product-list,product, product-detail and review-list components

--- oshare-front-app/src/app/app.module.ts
+++ oshare-front-app/src/app/app.module.ts
@@ -8,21 +8,21 @@ import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProductComponent } from './product/product.component';
 import { ProfileHeaderComponent } from './profile/profile-header/profile-header.component';
-import { PostListComponent } from './post-list/post-list.component';
+// import { PostListComponent } from './post-list/post-list.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { ProductItemComponent } from './product-list/product-item/product-item.component';
+// import { ProductItemComponent } from './product/product-/product-item.component';
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 import { ReviewListComponent } from './product/review-list/review-list.component';
 import { ReviewItemComponent } from './product/review-list/review-item/review-item.component';
-import { PostItemComponent } from './post-list/post-item/post-item.component';
+// import { PostItemComponent } from './post-list/post-item/post-item.component';
 import { HomeSearchComponent } from './home-search/home-search.component';
 import { SearchResultComponent } from './search-result/search-result.component';
 import { CartComponent } from './cart/cart.component';
 import { ProductCountListComponent } from './cart/product-count-list/product-count-list.component';
 import { ProductCountItemComponent } from './cart/product-count-item/product-count-item.component';
 import { CheckoutComponent } from './checkout/checkout.component';
-import { PostComponent } from './post/post.component';
-import { PostDetailComponent } from './post/post-detail/post-detail.component';
+// import { PostComponent } from './post/post.component';
+// import { PostDetailComponent } from './post/post-detail/post-detail.component';
 
 @NgModule({
   declarations: [
@@ -33,21 +33,21 @@ import { PostDetailComponent } from './post/post-detail/post-detail.component';
     ProfileComponent,
     ProductComponent,
     ProfileHeaderComponent,
-    PostListComponent,
+    // PostListComponent,
     ProductListComponent,
-    ProductItemComponent,
+    // ProductItemComponent,
     ProductDetailComponent,
     ReviewListComponent,
     ReviewItemComponent,
-    PostItemComponent,
+    // PostItemComponent,
     HomeSearchComponent,
     SearchResultComponent,
     CartComponent,
     ProductCountListComponent,
     ProductCountItemComponent,
     CheckoutComponent,
-    PostComponent,
-    PostDetailComponent
+    // PostComponent,
+    // PostDetailComponent
   ],
   imports: [
     BrowserModule
