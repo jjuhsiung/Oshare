3deaeb65b2b166e4d439affeb989e5b281223e63
Sherylhs <sherrybear1280@gmail.com>
2020-03-31 14:59:43 -0400

    add user service

---
+++ oshare-front-app/src/app/_services/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private httpOptions: any;
+
+  public username: string;
+
+  constructor() { }
+}
