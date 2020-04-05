aa7b13d93848bbfe411e5c0876f912a718daccc0
aduyinuo <yinuod@andrew.cmu.edu>
2020-04-05 11:49:37 -0400

    add postId field to post model

--- oshare-front-app/src/app/_models/post.model.ts
+++ oshare-front-app/src/app/_models/post.model.ts
@@ -1,13 +1,15 @@
 import { OComment } from '../shared/post-list/OComment.model';
 
 export class Post {
+    public postId: number;
     public imagePath: string;
     public postDate: Date;
     public likes: Number; 
     public postText: string; 
     public comments: OComment[]; 
 
-    constructor(imagePath: string, postDate: Date, likes: Number, postText: string, comments: OComment[]) {
+    constructor(postId: number, imagePath: string, postDate: Date, likes: Number, postText: string, comments: OComment[]) {
+        this.postId = postId;
         this.imagePath = imagePath;
         this.postDate = postDate;
         this.likes = likes;
