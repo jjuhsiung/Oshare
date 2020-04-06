f7f12a1c6640ba6c58dd33f1a1989de182f936a6
aduyinuo <yinuod@andrew.cmu.edu>
2020-04-06 16:51:36 -0400

    fix typos

--- django/webapps/oshare/views.py
+++ django/webapps/oshare/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
@@ -13,25 +12,8 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     # authentication_classes = (TokenAuthentication,)
     # permission_classes = (IsAuthenticated,)
-=======
-from django.shortcuts import render
-from rest_framework import viewsets
-from oshare.models import *
-
-from oshare.serializers import *
-
-
-# Create your views here.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = UserModel.objects.all()
-    serializer_class = UserModelSerializer
->>>>>>> yinuod
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
-<<<<<<< HEAD
     serializer_class = PostSerializer
-=======
-    serializer_class = PostSerializer
->>>>>>> yinuod
