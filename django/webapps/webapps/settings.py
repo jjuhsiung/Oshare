1dea99877453e10ca2b593c66daeb6b0d847b5f7
Sherylhs <sherrybear1280@gmail.com>
2020-04-06 00:30:19 -0400

    add virtaul env for local run

--- django/webapps/webapps/settings.py
+++ django/webapps/webapps/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'webapps.urls'
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'webapps.wsgi.application'
+# WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/bear/Desktop/webApp/team35/venv/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
