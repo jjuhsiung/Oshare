0b189a6c47b4fada6773ade7a470983dc9bdc48b
Sherylhs <sherrybear1280@gmail.com>
2020-04-08 22:06:26 -0400

    edit profile info from backend data, TODO, post + delete nav-bar

---
+++ oshare-front-app/src/app/_services/profile.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+  response_object = null;
+  baseurl = "http://127.0.0.1:8000";
+  httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' })
+  httpClient: any;
+
+  constructor(private http: HttpClient) { }
+
+  getProfileById(id): Observable<any> {
+    this.response_object = this.http.get(this.baseurl + '/profile/', { headers: this.httpHeaders });
+    return this.response_object;
+  }
+}
