a3f035178f883f1b370539cde78c656b7458de7c
Tian Su <213152046@seu.edu.cn>
2020-04-09 03:44:58 -0400

    change the UI of product-detail page and add all the brands in brandlist

--- oshare-front-app/src/app/shared/search-bar/search-bar.component.ts
+++ oshare-front-app/src/app/shared/search-bar/search-bar.component.ts
@@ -13,6 +13,15 @@ export class SearchBarComponent implements OnInit {
   brand = '';
   price = '';
   ProductType = '';
+  brandlist: string[] = ['almay', 'alva', 'anna sui', 'annabelle', 'benefit', 'boosh',
+'burt\'s bees', 'butter london', 'c\'est moi', 'cargo cosmetics', 'china glaze', 'clinique',
+'coastal classic creation', 'colourpop', 'covergirl', 'dalish', 'deciem', 'dior', 'dr. hauschka',
+'e.l.f.', 'essie', 'fenty', 'glossier', 'green people', 'iman', 'l\'oreal', 'lotus cosmetics usa', 'maia\'s mineral galaxy',
+'marcelle', 'marienatie', 'maybelline', 'milani', 'mineral fusion', 'misa', 'mistura', 'moov',
+'nudus', 'nyx', 'orly', 'pacifica', 'penny lane organics', 'physicians formula', 'piggy paint',
+'pure anada', 'rejuva minerals', 'revlon', 'sally b\'s skin yummies', 'salon perfect', 'sante',
+'sinful colours', 'smashbox', 'stila', 'suncoat', 'w3llpeople', 'wet n wild', 'zorah',
+'zorah biocosmetiques'];
   query = new ProductQuery();
   constructor(private api: ProductService) {
   }
