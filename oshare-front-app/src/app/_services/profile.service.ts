0883b3ca7179e14a7ac19d122758144a0a27a49a
JuiHsuan-Kuo <fionakuo26@gmail.com>
2020-04-14 00:40:41 -0400

    remove redundant functions

--- oshare-front-app/src/app/_services/profile.service.ts
+++ oshare-front-app/src/app/_services/profile.service.ts
@@ -13,10 +13,10 @@ export class ProfileService {
 
   constructor(private http: HttpClient) { }
 
-  getProfileById(id): Observable<any> {
-    this.response_object = this.http.get(this.baseurl + '/profile/', { headers: this.httpHeaders });
-    return this.response_object;
-  }
+  // getProfileById(id): Observable<any> {
+  //   this.response_object = this.http.get(this.baseurl + '/profile/', { headers: this.httpHeaders });
+  //   return this.response_object;
+  // }
 
   getProfileByURL(profileURL: string): Observable<any> {
     return this.http.get<any>(profileURL);
@@ -33,10 +33,10 @@ export class ProfileService {
     return this.http.patch<any>(this.baseurl + '/update_user/' + id + '/', formdata);
   }
 
-  editProfile(formdata: any): Observable<any> {
-    console.log("--edit profile--")
-    console.log(formdata)
-    var id = localStorage.getItem("userId");
-    return this.http.patch<any>(this.baseurl + '/profile/' + id + '/', formdata);
-  }
+  // editProfile(formdata: any): Observable<any> {
+  //   console.log("--edit profile--")
+  //   console.log(formdata)
+  //   var id = localStorage.getItem("userId");
+  //   return this.http.patch<any>(this.baseurl + '/profile/' + id + '/', formdata);
+  // }
 }
