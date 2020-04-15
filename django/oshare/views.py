deca89870ac6d61464d428672c2daa5b3e1cc4be
Tian Su <213152046@seu.edu.cn>
2020-04-15 18:29:46 -0400

    add product rating field and show rating as stars

--- django/oshare/views.py
+++ django/oshare/views.py
@@ -174,8 +174,12 @@ def update_products_view(request: HttpRequest) -> JsonResponse:
             Product.objects.get(id=x['id'])
         except Product.DoesNotExist:
             price = 0.0
+            print(x)
             if x['price'] != None:
                 price = float(x['price'])
+            rating = 0.0
+            if x['rating']!=None:
+                rating= float(x['rating'])
             new_product = Product(
                 id=int(x['id']),
                 name=x['name'],
@@ -187,8 +191,9 @@ def update_products_view(request: HttpRequest) -> JsonResponse:
                 currency=x['currency'],
                 img_link=x['image_link'],
                 description=x['description'],
-                rating=x['rating'],
+                rating=rating,
             )
+            print("rating:",new_product.rating)
             new_product.save()
     return JsonResponse({})
 
