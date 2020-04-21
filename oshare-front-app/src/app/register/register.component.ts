df850b9e5a60efec040460d9524db7bbc43428e0
JuiHsuan-Kuo <fionakuo26@gmail.com>
2020-04-21 17:11:23 -0400

    edit email regex pattern

--- oshare-front-app/src/app/register/register.component.ts
+++ oshare-front-app/src/app/register/register.component.ts
@@ -86,7 +86,7 @@ export class RegisterComponent implements OnInit {
       password: ['', Validators.required],
       first_name: ['', [Validators.required, Validators.maxLength(30)]],
       last_name: ['', [Validators.required, Validators.maxLength(150)]],
-      email: ['', [Validators.required, Validators.pattern(/[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g)]],
+      email: ['', [Validators.required, Validators.pattern(/^([a-z\d\.~]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/)]],
       phone: ['', [Validators.required, Validators.pattern("^[0-9]{10}$")]],
       address: ['', [Validators.required, Validators.maxLength(60)]],
     });
