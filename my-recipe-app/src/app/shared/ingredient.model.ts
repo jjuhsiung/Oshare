59430b1b98c88dcd490aafeacd0cd1e658a934e5
JuiHsuan-Kuo <fionakuo26@gmail.com>
2020-04-22 14:52:06 -0400

    remove

--- my-recipe-app/src/app/shared/ingredient.model.ts
+++
@@ -1,9 +0,0 @@
-export class Ingredient {
-    public name: string;
-    public amount: number;
-
-    constructor(name: string, amount: number) {
-        this.name = name;
-        this.amount = amount;
-    }
-}
